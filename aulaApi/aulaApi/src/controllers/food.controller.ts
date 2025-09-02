221f7a07e683455c9c7f29a300c17f948babba0b
elinton-souza <esc.vistorias@gmail.com>
2025-09-02 18:56:42 -0300

    Primeiro commit "Criação das páginas html/css: HOME, QUEM SOMOS e INDEX"

--- aulaApi/aulaApi/src/controllers/food.controller.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { Request, Response } from "express";
-
-const prisma = new PrismaClient();
-
-export const getFoods = async (req: Request, res: Response) => {
-  const foods = await prisma.food.findMany();
-  res.json(foods);
-};
--- aulaApi/aulaApi/src/routes/food.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getFoods } from "../controllers/food.controller";
-
-const foodRoutes = Router();
-
-foodRoutes.get("/", getFoods);
-
-export default foodRoutes;
